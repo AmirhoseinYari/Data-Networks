ff2f6d5a23653d61832330c86957edf76e9e810b
AmirhoseinYari <amirhoseinyari@icloud.com>
2022-06-15 19:32:27 +0430

    Part d finished

--- HW3/L2 System/d/L2_Part_d.cc
+++ HW3/L2 System/d/L2_Part_d.cc
@@ -32,219 +32,28 @@
 #include <string>
 #include <bits/stdc++.h>
 #include <stdio.h>
+
  
 using namespace ns3;
 using namespace std;
  
 NS_LOG_COMPONENT_DEFINE ("CsmaBridgeOneHopExample");
 
-// Creating shortcut for an integer pair
+// Creating shortcut for an integer pair and Dijkstra function
 typedef  pair<int, int> iPair;
-  
-// Structure to represent a graph
-struct Graph
-{
-    int V, E;
-    vector< pair<int, iPair> > edges;
-  
-    // Constructor
-    Graph(int V, int E)
-    {
-        this->V = V;
-        this->E = E;
-    }
-  
-    // Utility function to add an edge
-    void addEdge(int u, int v, int w)
-    {
-        edges.push_back({w, {u, v}});
-    }
-  
-    // Function to find MST using Kruskal's
-    // MST algorithm
-    vector<pair<double, pair<int, int>>> kruskalMST();
-};
-  
-// To represent Disjoint Sets
-struct DisjointSets
-{
-    int *parent, *rnk;
-    int n;
-  
-    // Constructor.
-    DisjointSets(int n)
-    {
-        // Allocate memory
-        this->n = n;
-        parent = new int[n+1];
-        rnk = new int[n+1];
-  
-        // Initially, all vertices are in
-        // different sets and have rank 0.
-        for (int i = 0; i <= n; i++)
-        {
-            rnk[i] = 0;
-  
-            //every element is parent of itself
-            parent[i] = i;
-        }
-    }
-  
-    // Find the parent of a node 'u'
-    // Path Compression
-    int find(int u)
-    {
-        /* Make the parent of the nodes in the path
-           from u--> parent[u] point to parent[u] */
-        if (u != parent[u])
-            parent[u] = find(parent[u]);
-        return parent[u];
-    }
-  
-    // Union by rank
-    void merge(int x, int y)
-    {
-        x = find(x), y = find(y);
-  
-        /* Make tree with smaller height
-           a subtree of the other tree  */
-        if (rnk[x] > rnk[y])
-            parent[y] = x;
-        else // If rnk[x] <= rnk[y]
-            parent[x] = y;
-  
-        if (rnk[x] == rnk[y])
-            rnk[y]++;
-    }
-};
-  
- /* Functions returns vector of the MST*/  
-vector< pair<double, pair<int, int> >> Graph::kruskalMST()
-{
-    double mst_wt = 0; // Initialize result
-    vector< pair<double, pair<int, int> >> out; // vector of the tree
+vector< pair<double, pair<int, int> >> dijkstra(double G[6][6],int n,int startnode);
 
-    // Sort edges in increasing order on basis of cost
-    sort(edges.begin(), edges.end());
-  
-    // Create disjoint sets
-    DisjointSets ds(V);
-  
-    // Iterate through all sorted edges
-    vector< pair<int, iPair> >::iterator it;
-    for (it=edges.begin(); it!=edges.end(); it++)
-    {
-        int u = it->second.first;
-        int v = it->second.second;
-        double w = it->first;
-  
-        int set_u = ds.find(u);
-        int set_v = ds.find(v);
-  
-        // Check if the selected edge is creating
-        // a cycle or not (Cycle is created if u
-        // and v belong to same set)
-        if (set_u != set_v)
-        {
-            // Current edge will be in the MST
-            // so print it
-            //cout << u << " ---- " << v << endl;
-            out.push_back({w, {u, v}}); // nodes of the tree
-  
-            // Update MST weight
-            mst_wt += it->first;
-  
-            // Merge two sets
-            ds.merge(set_u, set_v);
-        }
-    }
-  
-    return out; //vector, links of tree
-}
-
-#define INFINITY 9999
-#define max 6
-void dijkstra(int G[max][max],int n,int startnode);
 int main() {
-   int G[max][max]={{0,1,0,3,10},
-                    {1,0,5,0,0},
-                    {0,5,0,2,1},
-                    {3,0,2,0,6},
-                    {10,0,1,6,0}
-                    };
-   int n=5;
-   int u=0;
-   dijkstra(G,n,u);
-   return 0;
-}
-void dijkstra(int G[max][max],int n,int startnode) {
-   int cost[max][max],distance[max],pred[max];
-   int visited[max],count,mindistance,nextnode,i,j;
-   for(i=0;i<n;i++)
-      for(j=0;j<n;j++)
-   if(G[i][j]==0)
-      cost[i][j]=INFINITY;
-   else
-      cost[i][j]=G[i][j];
-   for(i=0;i<n;i++) {
-      distance[i]=cost[startnode][i];
-      pred[i]=startnode;
-      visited[i]=0;
-   }
-   distance[startnode]=0;
-   visited[startnode]=1;
-   count=1;
-   while(count<n-1) {
-      mindistance=INFINITY;
-      for(i=0;i<n;i++)
-         if(distance[i]<mindistance&&!visited[i]) {
-         mindistance=distance[i];
-         nextnode=i;
-      }
-      visited[nextnode]=1;
-      for(i=0;i<n;i++)
-         if(!visited[i])
-      if(mindistance+cost[nextnode][i]<distance[i]) {
-         distance[i]=mindistance+cost[nextnode][i];
-         pred[i]=nextnode;
-      }
-      count++;
-   }
-   for(i=0;i<n;i++)
-   if(i!=startnode) {
-      cout<<"\nDistance of node"<<i<<"="<<distance[i];
-      cout<<"\nPath="<<i;
-      j=i;
-      do {
-         j=pred[j];
-         cout<<"<-"<<j;
-      }while(j!=startnode);
-   }
-}
- /*
-int main (int argc, char **argv) {
-  int V = 6, E = 12;
-  Graph g(V, E);
-  
-  //  making the graph
-  g.addEdge(1, 2, 50);
-  g.addEdge(1, 3, 50);
-  g.addEdge(1, 4, 186.6);
-
-  g.addEdge(2, 3, 50);
-  g.addEdge(2, 5, 100);
-  g.addEdge(2, 6, 145.5);
-
-  g.addEdge(3, 4, 193.2);
-  g.addEdge(3, 5, 145.5);
-  g.addEdge(3, 6, 186.6);
-
-  g.addEdge(4, 5, 50);
-  g.addEdge(4, 6, 50);
-
-  g.addEdge(5, 6, 50);
-
-  vector< pair <double, pair<int, int> >> out = g.kruskalMST(); //MST Links
+  double G[6][6]={{0    ,50   , 50   ,186.6,0    , 0     },
+                  {50   ,0    , 50   ,0    ,100  , 145.5 },
+                  {50   ,50   , 0    ,193.2,145.5, 186.6 },
+                  {186.6,0    , 193.2,0    ,50   , 50    },
+                  {0    ,100  , 145.5,50   ,0    , 145.5 },
+                  {0    ,145.5, 186.6,50   ,145.5, 0     }
+                  };
+  int n=6;
+  int u=0;
+  vector< pair <double, pair<int, int> >> out = dijkstra(G,n,u);
 
   NS_LOG_INFO ("Create nodes.");
   Ptr<Node> c1 = CreateObject<Node> ();
@@ -376,7 +185,7 @@ int main (int argc, char **argv) {
   // C1 -> C6
   OnOffHelper onoff1 ("ns3::UdpSocketFactory",
                      Address (InetSocketAddress (Ipv4Address ("10.10.18.6"), port)));
-  onoff1.SetConstantRate (DataRate ("1000kb/s"));
+  onoff1.SetConstantRate (DataRate ("10000kb/s"));
   onoff1.SetAttribute("StartTime", TimeValue(Seconds(0.0)));
  
   ApplicationContainer app1 = onoff1.Install (c1);
@@ -387,7 +196,7 @@ int main (int argc, char **argv) {
   // C2 -> C7
   OnOffHelper onoff2 ("ns3::UdpSocketFactory",
                      Address (InetSocketAddress (Ipv4Address ("10.10.18.7"), port)));
-  onoff2.SetConstantRate (DataRate ("1000kb/s"));
+  onoff2.SetConstantRate (DataRate ("10000kb/s"));
   onoff2.SetAttribute("StartTime", TimeValue(Seconds(0.0)));
  
   ApplicationContainer app2 = onoff2.Install (c2);
@@ -469,5 +278,48 @@ int main (int argc, char **argv) {
   
   Simulator::Destroy ();
   NS_LOG_INFO ("Done."); 
+   return 0;
 }
-*/
+
+vector< pair<double, pair<int, int> >> dijkstra(double G[6][6],int n,int startnode) {
+   double cost[6][6],distance[6],pred[6];
+   int visited[6],count,mindistance,nextnode,i,j;
+   vector< pair<double, pair<int, int> >> out; // vector of the tree
+   for(i=0;i<n;i++)
+      for(j=0;j<n;j++)
+   if(G[i][j]==0)
+      cost[i][j]=9999; //inf
+   else
+      cost[i][j]=G[i][j];
+   for(i=0;i<n;i++) {
+      distance[i]=cost[startnode][i];
+      pred[i]=startnode;
+      visited[i]=0;
+   }
+   distance[startnode]=0;
+   visited[startnode]=1;
+   count=1;
+   while(count<n-1) {
+      mindistance=9999; //inf
+      for(i=0;i<n;i++)
+         if(distance[i]<mindistance&&!visited[i]) {
+         mindistance=distance[i];
+         nextnode=i;
+      }
+      visited[nextnode]=1;
+      for(i=0;i<n;i++)
+         if(!visited[i])
+      if(mindistance+cost[nextnode][i]<distance[i]) {
+         distance[i]=mindistance+cost[nextnode][i];
+         pred[i]=nextnode;
+      }
+      count++;
+   }
+   for(i=0;i<n;i++)
+   if(i!=startnode) {
+    j = pred[i];
+    //cout << "Link from " << i+1 << " to " << j+1 << "  " << G[i][j] << " m" << endl;
+    out.push_back({G[i][j], {i+1, j+1}}); // nodes of the tree
+   }
+   return out;
+}
